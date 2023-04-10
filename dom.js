b3b0022855b169e5d7706ca3e55bc020cbf1de21
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-04-10 09:16:53 +0530

    edit and delete functionality added

--- dom.js
+++ dom.js
@@ -7,17 +7,16 @@ const msg = document.querySelector(".msg");  //for display error
 const userList = document.querySelector("#users");  //for adding no. of users
 const div = document.querySelector(".container");
 
-let i = 0;
-const students = []; //create the big array which is store the data of multiple user by pushing objects
+let editIndex = -1;  //assign for both boolean(true) and index purpose
 myForm.addEventListener("submit", onSubmit);
+
 function onSubmit(e) {
   e.preventDefault();
-  i = i + 1;  //this integer count assign as a id for each users
 
   if (nameInput.value === "" || emailInput.value === "" || mobileInput.value === "") {
-    msg.classList.add("error"); //this gives error background color which is added from CSS style sheet
+    msg.classList.add("error");  //this gives error background color which is added from CSS style sheet
     //msg.innerHTML = "<h6>Please enter all text field *</h6> ";  //text display as a error
-    msg.innerText = "Please enter all text field *";  //text display as a error
+    msg.innerText = "Please enter all text field * ";  //text display as a error
     setTimeout(() => {
       msg.remove();
     }, 3500); //after 3Sec. error will be dissappeared
@@ -29,91 +28,145 @@ function onSubmit(e) {
       email: emailInput.value,
       mobile: mobileInput.value
     };
-    students.push(object);
-    //while setItem in localStorage then convert whole object into string
-    localStorage.setItem("Students1", JSON.stringify(students));
-    //while getItem from localStorage then convert whole string into object
-    console.log(JSON.stringify(students))
-    //let localStudents = localStorage.getItem("Students1");
-    //console.log(JSON.parse(localStudents));
-    showUserOnScreen(object);
-  }
 
-  function showUserOnScreen(object) {
-    const liList = document.createElement("li");
-    liList.innerText = (object.name + "  -  " + object.email + "  -  " + object.mobile);
+    let localStudents = JSON.parse(localStorage.getItem("Students1"));
 
-    const deleteButton = document.createElement("input");
-    deleteButton.type = "button";
-    deleteButton.className = "btn delete float-right";
-    deleteButton.value = "Delete";
-    deleteButton.id = i;
-
-    //onclick on delete button pass a unique id to delete button
-    deleteButton.onclick = (e) => {
-      removeLocalStorageItems(this.id);
-      removeItem(e);
-    }
-
-    liList.appendChild(deleteButton)
-    userList.appendChild(liList);
-    // console.log(userList);
-    div.appendChild(userList);
-
-  }
-
-  // Remove item
-  function removeItem(e) {
-    console.log(e.target.classList.contains('delete'));
-
-    if (e.target.classList.contains('delete')) {
-      if (confirm('Are You Sure?')) {
-        var li = e.target.parentElement;
-        userList.removeChild(li);
+    if (editIndex == -1) {
+      if (localStudents == null) {
+        localStudents = []
       }
+      localStudents.push(object);
+      showUserOnScreen(object);
     }
+    else {
+      localStudents[editIndex] = object;
+      console.log(localStorage.getItem("Students1"))
+
+      let textNode = document.createTextNode(object.name + " - " + object.email + " - " + object.mobile);
+      userList.children[editIndex].replaceChild(textNode, userList.children[editIndex].firstChild)
+
+      editIndex = -1;
+    }
+
+    localStorage.setItem("Students1", JSON.stringify(localStudents));
+
   }
 
-  //clicked_id = this.id
-  function removeLocalStorageItems(clicked_id) {
-    //students.removeItem(clicked_id);
-    students.splice(clicked_id, 1);
-    localStorage.setItem("Students1", JSON.stringify(students));
-    //while getItem from localStorage then convert whole string into object
-    //console.log(JSON.stringify(students))
-  }
-
-
-  /*
-   function showUserOnScreen() {
- 
-     let localStudents = localStorage.getItem("Students1");
-     let students = JSON.parse(localStudents)
- 
-     students.forEach(e => {
-       console.log(e.name, e.email, e.mobile)
- 
-       const liList = document.createElement("li");
-       liList.innerText = (e.name + "  -  " + e.email + "  -  " + e.mobile);
- 
-       const deleteButton = document.createElement("input");
-       deleteButton.type = "button";
-       deleteButton.className = "btn delete float-right";
-       deleteButton.value = "Delete";
-       deleteButton.onclick = () => {
-         localStorage.removeItem("Students1");
-         userList.removeChild(liList);
-       }
-       liList.appendChild(deleteButton)
-       userList.appendChild(liList);
-       console.log(userList);
-       div.appendChild(userList);
-     });
-   }
-   */
-
-  //To clear user details from dashboard after submit data
+  //To clear user details from dashboard text fieldafter submit data
   nameInput.value = "";
   emailInput.value = "";
   mobileInput.value = "";
 }
+
+
+function showUserOnScreen(object) {
+  const liList = document.createElement("li");
+  let textNode = document.createTextNode(object.name + " - " + object.email + " - " + object.mobile);
+
+  //Add Delete items functionality
+  const deleteButton = document.createElement("input");
+  deleteButton.type = "button";
+  deleteButton.className = "btn delete float-right";
+  deleteButton.value = "Delete";
+
+  deleteButton.onclick = (e) => {
+    if (confirm('Are You Sure?')) {
+      removeLocalStorageItems(e);
+      removeItem(e);
+    }
+  }
+
+  //Add Edit items functionality
+  const editBtn = document.createElement("input");
+  editBtn.type = "button";
+  editBtn.className = "btn edit float-right";
+  editBtn.value = "edit";
+
+  //onclick on delete button pass a unique id to delete button
+  editBtn.onclick = (e) => {
+    showItem(e);
+  }
+
+  liList.appendChild(textNode);
+  liList.appendChild(deleteButton);
+  liList.appendChild(editBtn);
+  userList.appendChild(liList);
+  div.appendChild(userList);
+}
+
+//to get index of delete or edit button 
+getClickedIndex = (e) => {
+  var li = e.target.closest('li');
+  var nodes = Array.from(userList.children);
+
+  //or if you want to not depend on externally defined variables
+  var nodes = Array.from(li.closest('ul').children);
+  var index = nodes.indexOf(li);
+  return index;
+}
+
+// Remove item form documents
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    //var li = e.target.closest("li");  //or
+    let li = e.target.parentElement;  //means (e.target=delete button) kaa parent which is li 
+    userList.removeChild(li);
+  }
+}
+
+//remove/update items from local storage
+function removeLocalStorageItems(e) {
+  let clicked_id = getClickedIndex(e);
+
+  let localStudents = JSON.parse(localStorage.getItem("Students1"));
+
+  localStudents.splice(clicked_id, 1);
+  if (localStudents.length === 0) {
+    localStorage.removeItem("Students1");
+  }
+  else {
+    localStorage.setItem("Students1", JSON.stringify(localStudents));
+  }
+}
+
+//show items on textfiled when we click on the edit button
+function showItem(e) {
+  let clicked_id = getClickedIndex(e);
+
+  const storedData = localStorage.getItem(("Students1"));
+  const storedArray = JSON.parse(storedData);
+
+  const studentObj = storedArray[clicked_id];
+
+  nameInput.value = studentObj.name;
+  emailInput.value = studentObj.email;
+  mobileInput.value = studentObj.mobile;
+  editIndex = clicked_id;
+}
+
+
+/* function showAllUserOnScreen() {
+ 
+    let localStudents = localStorage.getItem("Students1");
+    let students = JSON.parse(localStudents)
+ 
+    students.forEach(e => {
+      console.log(e.name, e.email, e.mobile)
+ 
+      const liList = document.createElement("li");
+      liList.innerText = (e.name + "  -  " + e.email + "  -  " + e.mobile);
+ 
+      const deleteButton = document.createElement("input");
+      deleteButton.type = "button";
+      deleteButton.className = "btn delete float-right";
+      deleteButton.value = "Delete";
+      deleteButton.onclick = () => {
+        localStorage.removeItem("Students1");
+        userList.removeChild(liList);
+      }
+      liList.appendChild(deleteButton)
+      userList.appendChild(liList);
+      console.log(userList);
+      div.appendChild(userList);
+    });
+  }*/
